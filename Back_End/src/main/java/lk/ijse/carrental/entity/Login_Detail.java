8ee53b4719bdbbf913e30e43bdea4618fb970927
ImanAdithya <adithyaiman07@gmail.com>
2023-10-23 11:22:10 +0530

    content updated

--- Back_End/src/main/java/lk/ijse/carrental/entity/Login_Detail.java
+++ Back_End/src/main/java/lk/ijse/carrental/entity/Login_Detail.java
@@ -1,8 +1,24 @@
 package lk.ijse.carrental.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Login_Detail {
-    String loginID;
-    String userName;
-    String date;
-    String time;
+
+    @Id
+    private String loginID;
+    private String date;
+    private String time;
+    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name ="userName",referencedColumnName = "userName",nullable = false)
+    private String userName;
 }
--- Back_End/src/main/java/lk/ijse/carrental/entity/User.java
+++ Back_End/src/main/java/lk/ijse/carrental/entity/User.java
@@ -1,8 +1,22 @@
 package lk.ijse.carrental.entity;
 
-public class User {
-    String userName;
-    String password;
-    String role;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class User {
+    @Id
+    private String userName;
+    private String password;
+    private String role;
 }
